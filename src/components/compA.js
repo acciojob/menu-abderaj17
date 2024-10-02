5a6677f2a642d5e42ff27245c2042b490f1f7317
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2024-10-02 20:39:37 +0530

    cp

---
+++ src/components/compA.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const compA = (item) => {
+    const{title, price, desc, category, img}=item
+
+  return (
+    <div data-test-id={"menu-item-"+category} style={{display:"flex", border:"1px solid black", width:"500px"}}>
+        <img src={img} alt='broken'/>
+        <div>
+            <div>
+                <h3>{title}</h3>
+                <span>{price}</span>
+            </div>
+            <p>{desc}</p>
+        </div>
+
+    </div>
+  )
+}
+export default compA;
